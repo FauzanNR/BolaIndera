c41f2c6b7e81dba576f2bee8b75e1815c6628e3d
erikcontena <contenaerik@gmail.com>
2020-03-14 12:07:15 +0800

    Adapter, layout rc_view

--- app/src/main/java/com/bolaindera/Adapter.java
+++ app/src/main/java/com/bolaindera/Adapter.java
@@ -1,4 +1,40 @@
 package com.bolaindera;
 
-public class Adapter {
+import android.graphics.Movie;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ListViewHolder>{
+
+    @NonNull
+    @Override
+    public Adapter.ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.ListViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+        }
+    }
 }
--- app/src/main/java/com/bolaindera/MainActivity.java
+++ app/src/main/java/com/bolaindera/MainActivity.java
@@ -1,6 +1,8 @@
 package com.bolaindera;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Adapter adapter = new Adapter();
+        adapter.notify();
+        recyclerView.setAdapter(adapter);
     }
 }
